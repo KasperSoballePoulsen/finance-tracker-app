29d8ad0be58a36b153c778020cdbad71326088bf
Kasper Søballe Poulsen <kaspersoballe@gmail.com>
2025-06-04 23:25:12 +0200

    Added update transaction functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Header from "./components/Header"
-
-import CreateTransaction from "./components/CreateTransaction"
-import ViewTransactions from "./components/ViewTransactions"
+import Header from "./components/Header";
+import CreateTransaction from "./components/CreateTransaction";
+import ViewTransactions from "./components/ViewTransactions";
 import { Transaction } from "./types/Transaction";
-
-
-
 
 function App() {
   const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+  const [editingTransaction, setEditingTransaction] = React.useState<Transaction | null>(null);
 
   const fetchTransactions = async () => {
     const res = await fetch("/transactions");
@@ -23,11 +20,20 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <CreateTransaction onSaved={fetchTransactions} />
-      <ViewTransactions transactions={transactions} />
+      <Header />
+      <CreateTransaction
+        onSaved={() => {
+          fetchTransactions();
+          setEditingTransaction(null);
+        }}
+        transaction={editingTransaction}
+      />
+      <ViewTransactions
+        transactions={transactions}
+        onEditClick={(t) => setEditingTransaction(t)}
+      />
     </>
   );
 }
 
-export default App
+export default App;
